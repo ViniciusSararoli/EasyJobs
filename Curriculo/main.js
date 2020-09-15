5ece13a54c3ebd1220df541069eb9d9dd5b220d9
Vinicius Sararoli de Souza <viniciussararoli@hotmail.com>
2020-09-14 22:27:14 -0300

    14-09-2020 22h27

---
+++ Curriculo/main.js
@@ -0,0 +1,5 @@
+var deficiencia = document.querySelector('input[name = deficiencia]');
+
+  deficiencia.onclick = function comDeficiencia() {
+   deficiencia = document.getElementById('auditiva').style.display = 'block'; 
+}
