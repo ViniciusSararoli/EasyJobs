c9be72eb4d7c5197098b6d0feee36356f1d273f2
Vinicius Sararoli de Souza <viniciussararoli@hotmail.com>
2020-09-16 21:13:05 -0300

    16-09-2020

--- Curriculo/main.js
+++ Curriculo/main.js
@@ -1,26 +1,17 @@
-var deficiencia = document.querySelector
-('input[name = deficiencia]');
 
-deficiencia.onclick = function comDeficiencia() {
-  deficiencia = document.getElementById('defici').style.display = 'block'; 
-}
-  /*deficiencia.onclick = function comDeficiencia() {
-    if (deficiencia.onclick == true) {
-      deficiencia = document.getElementById('defici').style.display = 'block';   
-    }else{
-     deficiencia = document.getElementById('defice').style.display = 'none';  
-    };
-};comDeficiencia();*/
 
-/********************************************************************************** */
-var painelFormacao = document.querySelector('button#incluir');
+function mostrar(id) {
+  if (document.getElementById(id).style.display == 'block'){
+    document.getElementById(id).style.display = 'none';
+  }else{
+    document.getElementById(id).style.display = 'block';
+  } 
+};
 
-painelFormacao.onclick = function() {
-  painelFormacao = document.querySelector('div.dadosFormacao').style.display = 'block';
-}
 
 var fecharFormacao = document.querySelector('button#cancelar');
 
 fecharFormacao.onclick = function () {
   fecharFormacao = document.getElementById('dadosFormacao').style.display = 'none';
 }
+
